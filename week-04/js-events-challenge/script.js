62ab449366cbca8166ab7aebb5251be5f8a01a32
martybonacci <martybonacci@gmail.com>
2022-02-01 13:44:28 -0700

    js challenge answer

---
+++ week-04/js-events-challenge/script.js
@@ -0,0 +1,64 @@
+
+const findAndReplace = (theEvent) => {
+	theEvent.preventDefault()
+	let text =document.getElementById('text').innerText
+	console.log(text)
+	const searchTerm = document.getElementById('text-find').value
+	console.log(searchTerm)
+	const replaceTerm = document.getElementById('text-replace').value
+	console.log(replaceTerm)
+	const regularExpression = new RegExp(searchTerm, 'gi')
+	const newText = text.replace(regularExpression, replaceTerm)
+	console.log(newText)
+	document.getElementById('text').innerText = newText
+}
+
+let findAndReplaceForm = document.getElementById('find-and-replace-form')
+findAndReplaceForm.addEventListener('submit', findAndReplace)
+
+function shuffleWords(){
+	let paragraph = document.getElementById('shuffle').innerText
+	console.log(paragraph)
+	let words = paragraph.split(' ')
+	console.log(words)
+
+	for(let i = 0; i < words.length; i++) {
+		let randomNumber = Math.random()
+		console.log('randomNumber', randomNumber)
+		let decimalIndex = randomNumber * words.length
+		console.log('decimalIndex', decimalIndex)
+		let newLocation = Math.floor(decimalIndex)
+		console.log('newLocation', newLocation)
+		let currentWord = words[i]
+		words[i] = words[newLocation]
+		words[newLocation] = currentWord
+ 	}
+
+	const newParagraph = words.join(' ')
+	document.getElementById('shuffle').innerText = newParagraph
+}
+
+function shuffleLetters(){
+	let paragraph = document.getElementById('shuffle').innerText
+	console.log(paragraph)
+	let words = paragraph.split(' ')
+	console.log(words)
+
+	let newWords = words.map(word => {
+		let newWord = word.split('')
+		for(let i = 0; i < newWord.length; i++) {
+			let newLocation = Math.floor(Math.random() * newWord.length)
+			let currentLetter = newWord[i]
+			newWord[i] = newWord[newLocation]
+			newWord[newLocation] = currentLetter
+		}
+		return newWord.join('')
+	})
+
+	const newParagraph = newWords.join(' ')
+	document.getElementById('shuffle').innerText = newParagraph
+}
+
+
+
+
